dbf52c77466fbb4a03bbdb61bfb68e52bac471bd
Fardin Amin Arpon <fardinaminarpon@ieee.org>
2021-09-22 02:47:51 +0600

    Mobile App Appium test Automaion

---
+++ src/test/java/BasePage.java
@@ -0,0 +1,21 @@
+/*
+Appium Mobile App Calculator Automation Project
+         Tested By:  Fardin Amin Arpon
+
+ */
+
+import io.appium.java_client.android.AndroidDriver;
+import org.testng.annotations.AfterTest;
+
+import java.io.IOException;
+
+public class BasePage {
+    public AndroidDriver driver = AppLaunch.getInstance().driver;
+    public BasePage() throws IOException, InterruptedException {
+    }
+    @AfterTest
+    public void tearDown()
+    {
+        driver.quit();
+    }
+}
